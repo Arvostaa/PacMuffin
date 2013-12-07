8d076b1d6f40b202585ed5b9ae4ac2f3e85c6e1f
arvostaa <arvostaa@arvostaa-AMILO-Pro-V3515.(none)>
2013-12-07 12:56:21 +0100

    field.h, object.h added

---
+++ field.h
@@ -0,0 +1,20 @@
+/*
+ * field.h
+ *
+ *  Created on: Dec 7, 2013
+ *      Author: arvostaa
+ */
+
+#ifndef FIELD_H_
+#define FIELD_H_
+
+typedef enum FIELD_TYPE {
+
+	FIELD_WALL = 0,
+	FIELD_HIGLIGHT = 1,
+	FIELD_PATH = 2
+
+}fieldType_e;
+
+
+#endif /* FIELD_H_ */
---
+++ object.h
@@ -0,0 +1,13 @@
+/*
+ * object.h
+ *
+ *  Created on: Dec 7, 2013
+ *      Author: arvostaa
+ */
+
+#ifndef OBJECT_H_
+#define OBJECT_H_
+
+
+
+#endif /* OBJECT_H_ */
